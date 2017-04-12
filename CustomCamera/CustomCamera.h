e5ac95557a4414f24e6fb4c7f08bb713f042410f
Daman1208 <daman.bisoncode@gmail.com>
2017-04-12 15:54:46 +0530

    Initial Commit

---
+++ CustomCamera/CustomCamera.h
@@ -0,0 +1,19 @@
+//
+//  CustomCamera.h
+//  CustomCamera
+//
+//  Created by Daman on 12/04/17.
+//  Copyright © 2017 Tarsem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CustomCamera.
+FOUNDATION_EXPORT double CustomCameraVersionNumber;
+
+//! Project version string for CustomCamera.
+FOUNDATION_EXPORT const unsigned char CustomCameraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomCamera/PublicHeader.h>
+
+
